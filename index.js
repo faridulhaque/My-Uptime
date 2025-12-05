6db7ef4ee43775e6e335142ddf26cf00f9ec8279
= <=>
2025-12-05 13:49:22 +0600

    conditianl api hit for db addded

--- index.js
+++ index.js
@@ -16,17 +16,23 @@ const urls = [
   process.env.MY_UPTIME,
 ];
 
+let timer = 0;
+
 const hitApi = async () => {
   for (const url of urls) {
     try {
-      const res = await axios.get(url);
-      console.log(url, res.data);
-    } catch (e) {
+      const finalUrl = timer >= 86400000 ? url + "?q=db" : url;
+      const res = await axios.get(finalUrl);
+      console.log(finalUrl, res.data);
+    } catch (err) {
       console.log(url, "Error hitting API");
     }
   }
+
+  timer += 30000;
+  if (timer >= 86400000) timer = 0;
 };
 
-setInterval(hitApi, 30000)
+setInterval(hitApi, 30000);
 
 app.listen(3000);
