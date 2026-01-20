6bfaf59bc75854561b8d64997563e6100f97418f
= <=>
2026-01-20 11:45:20 +0600

    docker added

--- index.js
+++ index.js
@@ -18,21 +18,21 @@ const urls = [
 
 let timer = 0;
 
-const hitApi = async () => {
-  for (const url of urls) {
-    try {
-      const finalUrl = timer >= 86400000 ? url + "?q=db" : url;
-      const res = await axios.get(finalUrl);
-      console.log(finalUrl, res.data);
-    } catch (err) {
-      console.log(url, "Error hitting API");
-    }
-  }
+// const hitApi = async () => {
+//   for (const url of urls) {
+//     try {
+//       const finalUrl = timer >= 86400000 ? url + "?q=db" : url;
+//       const res = await axios.get(finalUrl);
+//       console.log(finalUrl, res.data);
+//     } catch (err) {
+//       console.log(url, "Error hitting API");
+//     }
+//   }
 
-  timer += 30000;
-  if (timer >= 86400000) timer = 0;
-};
+//   timer += 30000;
+//   if (timer >= 86400000) timer = 0;
+// };
 
-setInterval(hitApi, 30000);
+// setInterval(hitApi, 30000);
 
-app.listen(3000);
+app.listen(5000);
